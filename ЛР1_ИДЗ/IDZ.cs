1754c5df49035cceb115f4596a80468980a8dd10
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-02 21:03:58 +0300

    Добавьте файлы проекта.

---
+++ ЛР1_ИДЗ/IDZ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1_ИДЗ
+{
+    internal class IDZ
+    {
+        public static void DoTask()
+        {
+            Console.Write("Введите количество чисел: ");
+            int n = int.Parse(Console.ReadLine());
+            float [] numb = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите "+(i+1)+" число ");
+                numb[i] = float.Parse(Console.ReadLine());
+            }
+            Console.Write("Сумма минимального элемента и след.мин.элемента равна: ");
+            float min=numb.Min();
+            float min2 = float.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                if (min < numb[i] && numb[i]<min2)
+                {
+                    min2= numb[i];
+                }
+            }
+            Console.WriteLine(min + min2);
+        }
+    }
+}
---
+++ ЛР1_ИДЗ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР1_ИДЗ
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            IDZ.DoTask();
+        }
+    }
+}
